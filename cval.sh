9e71fcd3e40a0304bd3d8003e1d5d52e3bad6deb
Navya Tyagi <99486994+Navya003@users.noreply.github.com>
2023-09-18 13:34:11 +0530

    Create cval.sh

---
+++ cval.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+sbatch <<EOT
+#!/bin/bash
+#SBATCH --job-name=2000.cval
+#SBATCH --time=1-00:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=64000
+#SBATCH --output=2000.cval.out
+#SBATCH --account=lz25
+#SBATCH --export=NONE
+#SBATCH --partition=m3g
+#SBATCH --gres=gpu:V100:1
+##SBATCH --partition=genomics
+##SBATCH --qos=genomics
+#SBATCH --mail-user=ext-ntyagi@monash.edu
+#SBATCH --mail-type=ALL
+#======START=====
+source /projects/lz25/navyat/conda
+conda activate /projects/lz25/navyat/conda/envs/something
+
+date
+
+cross_validate \
+  prot.2000.512/train.parquet \
+  "parquet" \
+  --test prot.2000.512/test.parquet \
+  --valid prot.2000.512/valid.parquet \
+  --entity_name tyagilab \
+  --project_name prot \
+  --group_name 2000.51.cval \
+  --label_names "labels" \
+  --config_from_run tyagilab/prot/lm52lquq
+  
+
+date
+exit 0
+EOT
